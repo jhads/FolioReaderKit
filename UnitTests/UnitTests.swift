b40606401d4a779223b5db4ea60a027f716100d8
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-10-14 17:49:43 +0100

    Enable unit testing

---
+++ UnitTests/UnitTests.swift
@@ -0,0 +1,16 @@
+//
+//  UnitTests.swift
+//  UnitTests
+//
+//  Created by Vinicius Leal on 14/10/2020.
+//  Copyright © 2020 FolioReader. All rights reserved.
+//
+
+import XCTest
+
+class UnitTests: XCTestCase {
+
+    func testExample() {
+        
+    }
+}
