5e58396a496d9ed7099eaf4aa31b2931ca982cc0
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-10-19 17:10:28 +0100

    Fix initialiser failure on 'FolioReaderScript'

--- Source/FolioReaderScript.swift
+++ Source/FolioReaderScript.swift
@@ -9,44 +9,10 @@
 import WebKit
 
 class FolioReaderScript: WKUserScript {
-    
-    init(source: String) {
-        super.init(source: source,
-                   injectionTime: .atDocumentEnd,
-                   forMainFrameOnly: true)
+        
+    convenience init(source: String) {
+        self.init(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
     }
-    
-    static let bridgeJS: FolioReaderScript = {
-        let jsURL = Bundle.frameworkBundle().url(forResource: "Bridge", withExtension: "js")!
-        let jsSource = try! String(contentsOf: jsURL)
-        return FolioReaderScript(source: jsSource)
-    }()
-    
-    static let cssInjection: FolioReaderScript = {
-        let cssURL = Bundle.frameworkBundle().url(forResource: "Style", withExtension: "css")!
-        let cssString = try! String(contentsOf: cssURL)
-        return FolioReaderScript(source: cssInjectionSource(for: cssString))
-    }()
-    
-    static func cssInjection(overflow: String) -> FolioReaderScript {
-        let cssString = "html{overflow:\(overflow)}"
-        return FolioReaderScript(source: cssInjectionSource(for: cssString))
-    }
-    
-    private static func cssInjectionSource(for content: String) -> String {
-        let oneLineContent = content.components(separatedBy: .newlines).joined()
-        let source = """
-        var style = document.createElement('style');
-        style.type = 'text/css'
-        style.innerHTML = '\(oneLineContent)';
-        document.head.appendChild(style);
-        """
-        return source
-    }
-    
-}
-
-extension WKUserScript {
     
     func addIfNeeded(to webView: WKWebView?) {
         guard let controller = webView?.configuration.userContentController else { return }
@@ -58,5 +24,34 @@ extension WKUserScript {
         if alreadyAdded { return }
         controller.addUserScript(self)
     }
+}
+
+struct ScriptSource {
+    static let bridgeJS: String = {
+        let jsURL = Bundle.frameworkBundle().url(forResource: "Bridge", withExtension: "js")!
+        let jsSource = try! String(contentsOf: jsURL)
+        return jsSource
+    }()
     
+    static let cssInjection: String = {
+        let cssURL = Bundle.frameworkBundle().url(forResource: "Style", withExtension: "css")!
+        let cssString = try! String(contentsOf: cssURL)
+        return cssInjectionSource(for: cssString)
+    }()
+    
+    static func cssInjection(overflow: String) -> String {
+        let cssString = "html{overflow:\(overflow)}"
+        return ScriptSource.cssInjectionSource(for: cssString)
+    }
+    
+    static private func cssInjectionSource(for content: String) -> String {
+        let oneLineContent = content.components(separatedBy: .newlines).joined()
+        let source = """
+        var style = document.createElement('style');
+        style.type = 'text/css'
+        style.innerHTML = '\(oneLineContent)';
+        document.head.appendChild(style);
+        """
+        return source
+    }
 }
